e5b9826ebb5c968cf75d62ae920665825387f83e
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-23 20:06:58 -0300

    Principais mudaças

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready( function() {
+    ajusteMargemMain();
+});
+
+function ajusteMargemMain() {
+    main = document.getElementById("container");
+    fundo = document.getElementById("fundo");
+
+    main.style.marginTop = "50px";
+};
