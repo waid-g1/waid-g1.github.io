64ac2ccff9e796ace373104e08bcc44fa06e9c0d
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-09-23 22:41:52 -0300

    centralizando navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 jQuery(document).ready( function() {
     ajusteMargemMain();
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        if (window.innerWidth <= 767.98)
+            $('html,body').animate({scrollTop:$(this.hash).offset().top-105}, 600);
+        else
+            $('html,body').animate({scrollTop:$(this.hash).offset().top-65}, 600);
+   });
 });
 
 function ajusteMargemMain() {
